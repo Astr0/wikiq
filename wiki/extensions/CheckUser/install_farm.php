c3a4508f89022012aed96c1cc248e9de243f194d
astr0 <aastr00@gmail.com>
2013-04-28 16:21:44 +0300

    Possibly fixed farm scripts.

--- wiki/extensions/CheckUser/install_farm.php
+++ wiki/extensions/CheckUser/install_farm.php
@@ -1,5 +1,5 @@
 <?php
-# update_farm.php script
+# install_farm.php script
 $_SERVER['SERVER_NAME'] = $argv[1];
 $_SERVER['DOCUMENT_ROOT']=  $argv[2]; #optional
-require "update.php";
+require "install.php";
