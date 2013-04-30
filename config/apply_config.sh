eb5122236d69e020b66ea63e8f80ee5c0d8bb286
astr0 <aastr00@gmail.com>
2013-04-30 13:48:25 +0300

    removed lsearchd deamon

--- config/apply_config.sh
+++ config/apply_config.sh
@@ -1,7 +1,4 @@
 /bin/cp -f httpd.conf /etc/httpd/conf/
 /bin/cp -f squid.conf /etc/squid/
-/bin/cp -f lsearchd /etc/init.d/
-chmod 755 /etc/init.d/lsearchd
-/etc/init.d/lsearchd restart
 service httpd restart
 service squid restart
