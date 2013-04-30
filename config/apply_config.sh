8cfd32efed09116dba012472ac2e8f718bf62c22
astr0 <aastr00@gmail.com>
2013-04-30 13:42:37 +0300

    lsearchd deamon mode

--- config/apply_config.sh
+++ config/apply_config.sh
@@ -1,4 +1,7 @@
 /bin/cp -f httpd.conf /etc/httpd/conf/
 /bin/cp -f squid.conf /etc/squid/
+/bin/cp -f lsearchd /etc/init.d/
+chmod 755 /etc/init.d/lsearchd
+/etc/init.d/lsearchd restart
 service httpd restart
 service squid restart
